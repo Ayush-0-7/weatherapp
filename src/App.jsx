7053b85582bd52b5e437d10e435c3483bf3f2cd3
Ayush-0-7 <22052720@kiit.ac.in>
2024-06-23 21:37:15 +0530

    image change

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import { useState,useEffect } from 'react'
 import './App.css'
 import Left_m from './Components/Left_m'
 import Right_m from './Components/Right_m'
-import loading from '../public/WeatherIcons.gif'
+import loading from './images/WeatherIcons.gif'
 import { useSelector } from 'react-redux'
 function App() {
   const loader = useSelector(state=>state.loader);
   return (
     <>
         <div>
-        <img src="../public/background1.jpg" alt="Full Screen Image" className="absolute inset-0 object-cover w-full h-full brightness-70"/>
+        <img src="./src/images/background1.jpg" alt="Full Screen Image" className="absolute inset-0 object-cover w-full h-full brightness-70"/>
         <div id='main' className=' w-full h-full md:h-[75%] md:w-[60%] rounded-md flex'>
           {
             loader == false ?
